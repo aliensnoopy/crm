ffefc625618e785e1d5310f8b9d35e637b052a85
kyleyu <kyleyu_work@163.com>
2021-03-13 13:29:17 +0800

    Polish basic utils.

--- src/main/java/com/yiyi/crm/base/BaseMapper.java
+++ src/main/java/com/yiyi/crm/base/BaseMapper.java
@@ -1,80 +1,76 @@
 package com.yiyi.crm.base;
 
-
-import org.springframework.dao.DataAccessException;
-
 import java.util.List;
+import org.springframework.dao.DataAccessException;
 
 /**
  * BaseMapper  基本方法定义
  */
-public interface BaseMapper<T,ID> {
-    /**
-     * 添加记录返回行数
-     * @param entity
-     * @return
-     */
-    public Integer insertSelective(T entity) throws DataAccessException;
+public interface BaseMapper<T, ID> {
+  /**
+   * Add record.
+   * @param entity
+   * @return The number of inserted rows.
+   */
+  public Integer insertSelective(T entity) throws DataAccessException;
 
-    /**
-     * 添加记录返回主键
-     * @param entity
-     * @return
-     */
-    public Integer insertHasKey(T entity) throws DataAccessException;
+  /**
+   * Insert record.
+   * @param entity
+   * @return
+   */
+  public Integer insertHasKey(T entity) throws DataAccessException;
 
-    /**
-     * 批量添加
-     * @param entities
-     * @return
-     */
-    public Integer insertBatch(List<T> entities) throws DataAccessException;
-
-
-    /**
-     * 根据id 查询详情
-     * @param id
-     * @return
-     */
-    public T selectByPrimaryKey(ID id) throws DataAccessException;
+  /**
+   * Batch insertion.
+   * @param entities
+   * @return
+   */
+  public Integer insertBatch(List<T> entities) throws DataAccessException;
 
 
-    /**
-     * 多条件查询
-     * @param baseQuery
-     * @return
-     */
-    public List<T> selectByParams(BaseQuery baseQuery) throws DataAccessException;
+  /**
+   * Query by id.
+   * @param id
+   * @return
+   */
+  public T selectByPrimaryKey(ID id) throws DataAccessException;
 
 
-    /**
-     * 更新单条记录
-     * @param entity
-     * @return
-     */
-    public Integer updateByPrimaryKeySelective(T entity) throws DataAccessException;
+  /**
+   * Query by multiple conditions.
+   * @param baseQuery
+   * @return
+   */
+  public List<T> selectByParams(BaseQuery baseQuery) throws DataAccessException;
 
 
-    /**
-     * 批量更新
-     * @param entities
-     * @return
-     */
-    public Integer updateBatch(List<T> entities) throws DataAccessException;
-
-    /**
-     * 删除单条记录
-     * @param id
-     * @return
-     */
-    public Integer deleteByPrimaryKey(ID id) throws DataAccessException;
-
-    /**
-     * 批量删除
-     * @param ids
-     * @return
-     */
-    public Integer deleteBatch(ID[] ids) throws DataAccessException;
+  /**
+   * Update single record.
+   * @param entity
+   * @return
+   */
+  public Integer updateByPrimaryKeySelective(T entity) throws DataAccessException;
 
 
+  /**
+   * Batch updates.
+   * @param entities
+   * @return
+   */
+  public Integer updateBatch(List<T> entities) throws DataAccessException;
+
+  /**
+   * Delete single record.
+   * @param id
+   * @return
+   */
+  public Integer deleteByPrimaryKey(ID id) throws DataAccessException;
+
+  /**
+   * Batch deletes.
+   * @param ids
+   * @return
+   */
+  public Integer deleteBatch(ID[] ids) throws DataAccessException;
 }
--- src/main/java/com/yiyi/crm/base/BaseQuery.java
+++ src/main/java/com/yiyi/crm/base/BaseQuery.java
@@ -1,23 +1,22 @@
 package com.yiyi.crm.base;
 
-
 public class BaseQuery {
-    private Integer page=1;
-    private Integer limit=10;
+  private Integer page = 1;
+  private Integer limit = 10;
 
-    public Integer getPage() {
-        return page;
-    }
+  public Integer getPage() {
+    return page;
+  }
 
-    public void setPage(Integer page) {
-        this.page = page;
-    }
+  public void setPage(Integer page) {
+    this.page = page;
+  }
 
-    public Integer getLimit() {
-        return limit;
-    }
+  public Integer getLimit() {
+    return limit;
+  }
 
-    public void setLimit(Integer limit) {
-        this.limit = limit;
-    }
+  public void setLimit(Integer limit) {
+    this.limit = limit;
+  }
 }

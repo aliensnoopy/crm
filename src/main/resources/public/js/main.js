dff72b05a3700b16e4675bff1556d55492c5948b
kyleyu <kyleyu_work@163.com>
2021-03-14 12:39:34 +0800

    Implement user logout function.

--- src/main/resources/public/js/main.js
+++ src/main/resources/public/js/main.js
@@ -3,9 +3,20 @@ layui.use(['element', 'layer', 'layuimini','jquery','jquery_cookie'], function (
         layer = layui.layer,
         $ = layui.jquery_cookie($);
 
-    // 菜单初始化
     $('#layuiminiHomeTabIframe').html('<iframe width="100%" height="100%" frameborder="0"  src="welcome"></iframe>')
     layuimini.initTab();
 
-
+    $(".login-out").click(function() {
+      layer.confirm("Confirm Exit?", {
+        icon: 3,
+        title: "Hint",
+        btn: ["Yes", "Nope"]
+      }, function(index) {
+        layer.close(index);
+        $.removeCookie("encryptedUserId", {domain: "localhost", path: "/crm"});
+        $.removeCookie("userName", {domain: "localhost", path: "/crm"});
+        $.removeCookie("trueName", {domain: "localhost", path: "/crm"});
+        window.location.href = ctx + "/index";
+      });
+    });
 });
